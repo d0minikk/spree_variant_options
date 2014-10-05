da8c7544bdd708e924cc617f530357d9d47a0418
Dominik Curyło <curylo.dominik@gmail.com>
2014-10-05 11:48:16 +0200

    new hash syntax and cleanup

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,10 +1,10 @@
 Spree::OptionValue.class_eval do
 
   has_attached_file :image,
-    :styles        => { mini: '32x32>', normal: '128x128>' },
-    :default_style => SpreeVariantOptions::VariantConfig[:option_value_default_style],
-    :url           => SpreeVariantOptions::VariantConfig[:option_value_url],
-    :path          => SpreeVariantOptions::VariantConfig[:option_value_path]
+    styles:        { mini: '32x32>', normal: '128x128>' },
+    default_style: SpreeVariantOptions::VariantConfig[:option_value_default_style],
+    url:           SpreeVariantOptions::VariantConfig[:option_value_url],
+    path:          SpreeVariantOptions::VariantConfig[:option_value_path]
 
   def has_image?
     image_file_name && !image_file_name.empty?
